f0ef4ba695ccce21827f7c13c076dd1bdaf155af
Thiago <thiago.barboza.p@gmail.com>
2021-07-21 20:28:46 -0300

    Commiting the files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
+const App = () => {
+  return (
+    <div>
+      <Title> Hello  World </Title>
+    </div>
+  )
+}
+
+export default App
+
